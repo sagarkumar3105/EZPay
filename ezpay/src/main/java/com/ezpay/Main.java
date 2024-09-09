22de7c1573ca10221f07a140effc6a027dd7dc8f
Sandarbha  Komujwar <101709644+Sandarbha-K@users.noreply.github.com>
2024-09-09 21:58:21 +0530

    Create Main.java

---
+++ ezpay/src/main/java/com/ezpay/Main.java
@@ -0,0 +1,14 @@
+package com.EzPayApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class EzPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzPayApplication.class, args);
+	}
+
+}
