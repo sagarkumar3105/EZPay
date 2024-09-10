1ee41add62af40961737aa695870672469bfc38b
Sandarbha  Komujwar <101709644+Sandarbha-K@users.noreply.github.com>
2024-09-10 16:15:27 +0530

    Update Main.java

--- ezpay/src/main/java/com/ezpay/Main.java
+++ ezpay/src/main/java/com/ezpay/Main.java
@@ -4,9 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+/**
+ * Main entry point for the EzPayApplication. This class bootstraps the Spring Boot application
+ * and initializes all the necessary components.
+ */
 @SpringBootApplication
 public class EzPayApplication {
-
+	/**
+     * Main method that serves as the entry point of the Spring Boot application.
+     * It starts the application by invoking {@link SpringApplication#run(Class, String[])}.
+     *
+     * @param args command-line arguments passed to the application
+     */
 	public static void main(String[] args) {
 		SpringApplication.run(EzPayApplication.class, args);
 	}
