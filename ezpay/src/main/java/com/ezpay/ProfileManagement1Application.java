bf9dd2d8498af00fd2308621a1cee49c85e913e7
Snehal Vats <snehal.vats430@gmail.com>
2024-09-13 11:58:29 +0530

    Add files via upload

---
+++ ezpay/src/main/java/com/ezpay/ProfileManagement1Application.java
@@ -0,0 +1,13 @@
+package com.ezpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileManagement1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileManagement1Application.class, args);
+	}
+
+}
